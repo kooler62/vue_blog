dc367f560a0145f3e5836e2a3d64cbb53fc21937
kooler62 <kooler62@gmail.com>
2020-11-27 15:10:21 +0200

    считывание активной категории и передача id категории в дочерний компонент

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -2,31 +2,52 @@
     <div>
         <h2>Categories:</h2>
         <ul>
-            <router-link v-for="category in headerCategories" active-class="active-category" :key="category.title" :to="`/category/${category.slug}`">{{ category.title }}</router-link>
+            <router-link @click.native="setActiveCategoryId(category.id)" v-for="category in headerCategories"
+                         active-class="active-category" :key="category.title" :to="`/category/${category.slug}`">{{
+                category.title }}
+            </router-link>
         </ul>
+        <PostsComponent :category_id="activeCategoryId"/>
+
     </div>
 </template>
 
 <script>
     import axios from 'axios';
+    import PostsComponent from "./PostsComponent";
 
     export default {
         name: "HeaderComponent",
+        components: {PostsComponent},
         data() {
             return {
-                headerCategories: null
+                activeCategoryId: null,
+                headerCategories: null,
             };
         },
+        methods: {
+            setActiveCategoryId(id) {
+                console.log(id);
+                this.activeCategoryId = id;
+            }
+        },
         mounted() {
+            //todo проверка если корень сайта или пустой масив с категориями
+
             axios
                 .get('https://blog.lpage.cc/api/v2/categories')
-                .then(response => (this.headerCategories = response.data.data));
+                .then(response => {
+                        this.headerCategories = response.data.data
+                        let categorySlug = this.$route.path.split('/').reverse()[0];
+                        this.activeCategoryId = this.headerCategories.find(el => el.slug === categorySlug).id
+                    }
+                );
         }
     }
 </script>
 
 <style scoped>
-.active-category{
-    font-weight: bold;
-}
+    .active-category {
+        font-weight: bold;
+    }
 </style>
---
+++ src/components/PostsComponent.vue
@@ -0,0 +1,14 @@
+<template>
+    <div><h1>{{ category_id }}</h1></div>
+</template>
+
+<script>
+    export default {
+        name: "PostsComponent",
+        props: ['category_id'],
+    }
+</script>
+
+<style scoped>
+
+</style>
