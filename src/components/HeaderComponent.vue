3366bee405cf558845f28f0bdd8ee8b71e566bdb
kooler62 <kooler62@gmail.com>
2020-11-27 14:46:00 +0200

    active category

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -2,7 +2,7 @@
     <div>
         <h2>Categories:</h2>
         <ul>
-            <router-link v-for="category in headerCategories" :key="category.title" :to="`/category/${category.slug}`">{{ category.title }}</router-link>
+            <router-link v-for="category in headerCategories" active-class="active-category" :key="category.title" :to="`/category/${category.slug}`">{{ category.title }}</router-link>
         </ul>
     </div>
 </template>
@@ -26,5 +26,7 @@
 </script>
 
 <style scoped>
-
+.active-category{
+    font-weight: bold;
+}
 </style>
