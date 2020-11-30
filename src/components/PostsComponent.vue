bf2041ba65dfe70a6a35146af9a413c04c9c23d8
kooler62 <kooler62@gmail.com>
2020-11-30 14:16:16 +0200

    default posts

--- src/components/PostsComponent.vue
+++ src/components/PostsComponent.vue
@@ -17,25 +17,35 @@
 
         data() {
             return {
-                posts: null,
+                posts: this.getPosts(),
                 category_id: null
             };
         },
         methods: {
-          getPosts (id) {
-              axios
-                  .get('https://blog.lpage.cc/api/v2/categories/'+ id)
-                  .then(response => {
-                       this.posts = response.data.posts
-                      }
-                  );
-          }
+            getPostsByCategoryId(id) {
+                axios
+                    .get('https://blog.lpage.cc/api/v2/categories/' + id)
+                    .then(response => {
+                            this.posts = response.data.posts
+                        }
+                    );
+            },
+            getPosts() {
+
+                axios
+                    .get('https://blog.lpage.cc/api/v2/posts')
+                    .then(response => {
+                            this.posts = response.data.data
+                        }
+                    );
+
+            }
         },
         mounted() {
             //todo проверка если корень сайта или пустой масив с категориями
             this.$root.$on('change-active-category', $event => {
                 this.category_id = $event;
-                this.getPosts(this.category_id)
+                this.getPostsByCategoryId(this.category_id)
             })
         }
     }
